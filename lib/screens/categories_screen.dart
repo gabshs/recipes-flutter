8adef013f32b800415d3795401704b7e4e5e3099
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-06-21 12:39:46 -0300

    feat: creates categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,30 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final categories = dummyCategories;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.white,
+        elevation: 0,
         title: const Text('Cardápios'),
       ),
-      body: const Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 32, top: 72, bottom: 20),
-            child: Text(
-              'Categorias',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 20),
+              child: Text(
+                'Categorias',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nunito',
+                ),
               ),
             ),
-          )
-        ],
+            Expanded(
+              child: GridView.builder(
+                itemCount: categories.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                  mainAxisExtent: 200,
+                ),
+                itemBuilder: (context, index) {
+                  final category = categories[index];
+
+                  return Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        height: 200,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.network(
+                            category.imageUrl,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(15),
+                            bottomRight: Radius.circular(15),
+                          ),
+                          child: Container(
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.all(10),
+                            width: 160,
+                            height: 45,
+                            color: Colors.black45,
+                            child: Text(
+                              category.title,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Nunito',
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.local_dining), label: ''),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_outlined), label: ''),
+      ]),
     );
   }
 }
