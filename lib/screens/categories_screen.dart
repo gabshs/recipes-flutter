af03e4487f843b7ed5064453d2f6c461aff4bca1
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-06-23 13:46:55 -0300

    refactor: return only  categories screen body

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -8,54 +8,41 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final categories = dummyCategories;
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const Text('Cardápios'),
-      ),
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 32, bottom: 20),
-              child: Text(
-                'Categorias',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Nunito',
-                ),
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 10, bottom: 20),
+            child: Text(
+              'Categorias',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Nunito',
               ),
             ),
-            Expanded(
-              child: GridView.builder(
-                itemCount: categories.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
-                  mainAxisExtent: 200,
-                ),
-                itemBuilder: (context, index) {
-                  final category = categories[index];
-
-                  return CategoryItem(category: category);
-                },
+          ),
+          Expanded(
+            child: GridView.builder(
+              itemCount: categories.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 15,
+                mainAxisExtent: 240,
               ),
-            )
-          ],
-        ),
+              itemBuilder: (context, index) {
+                final category = categories[index];
+
+                return CategoryItem(category: category);
+              },
+            ),
+          )
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.local_dining), label: ''),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border_outlined), label: ''),
-      ]),
     );
   }
 }
