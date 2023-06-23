3f9f1be21e59cc92dfed4ad984f6591b14051050
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-06-22 23:19:45 -0300

    feat: list meals

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -18,6 +18,115 @@ class CategoryMealsScreen extends StatelessWidget {
         title: Text(category.title),
         centerTitle: true,
       ),
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 20),
+              child: Text(
+                'Pratos',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Nunito',
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: categoryMeals.length,
+                itemBuilder: (context, index) {
+                  final meal = categoryMeals[index];
+                  return Card(
+                    elevation: 5,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 120,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.network(
+                                meal.imageUrl,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: 200,
+                                  child: Text(
+                                    meal.title,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.access_time,
+                                          size: 20,
+                                        ),
+                                        Text(
+                                          '${meal.duration} minutos',
+                                          style: const TextStyle(
+                                            fontFamily: 'Nunito',
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(
+                                      width: 30,
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.signal_cellular_alt,
+                                          size: 20,
+                                        ),
+                                        Text(
+                                          meal.complexityText,
+                                          style: const TextStyle(
+                                            fontFamily: 'Nunito',
+                                            fontSize: 12,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
