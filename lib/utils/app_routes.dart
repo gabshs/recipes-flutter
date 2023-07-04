29efdd1c70fa83031f0d85369b209f2d8f987af9
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-07-04 02:58:19 -0300

    feat: adds settings to app routes

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,5 +1,6 @@
 class AppRoutes {
   static const home = '/';
-  static const categoryMeals = 'category-meals';
-  static const mealDetail = 'meal-detail';
+  static const categoryMeals = '/category-meals';
+  static const mealDetail = '/meal-detail';
+  static const settings = '/settings';
 }
